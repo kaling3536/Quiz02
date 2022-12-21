1df41ac6602899b5f2ac38bab423e9c2ae813ab5
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-21 17:18:03 +0800

    Update J1.java

--- J1.java
+++ J1.java
@@ -1,46 +1,28 @@
 package test;
 import java.util.*;
-
-class Integer extends Exception {
-}
-class Integer1 extends Exception {
-}
-
-public class J1 {
+public class J1{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scn = new Scanner(System.in);
-		System.out.println("密碼輸入測試");
-		int num;
-		int num2;
-		int num3;
-		int num4;
-		try {
-			System.out.print("請輸入密碼:");
-			num = scn.nextInt();
-			System.out.print("請再輸入一次密碼:");
-			num2 = scn.nextInt();
-			System.out.print("請再輸入一次密碼:");
-			num3 = scn.nextInt();
-			if (num != num2)
-				throw new Integer();
-			else if (num3 != num2)
-				throw new Integer1();
+		Scanner sc=new Scanner(System.in);
+		int s1,s2;
+		System.out.print("請輸入密碼:");
+		s1=sc.nextInt();
+		int count=0;
+		for(;;)
+		{
+			System.out.print("請再次輸入密碼:");
+			s2=sc.nextInt();
+			if(s1==s2)
+				break;
 			else
-				System.out.println("密碼正確");
-				
-		} catch (Integer e) {
-			System.out.println("與第一次輸入的不同");
-		} 
-		catch (Integer1 e) {
-			System.out.println("與第一次輸入的不同");
-		} 
-		 catch (InputMismatchException e) {
-			System.out.println("輸入三次錯誤，程式停止");
+				count++;
+			if(count==3)
+				throw new RuntimeException("輸入三次錯誤！程式停止！");
+			else
+				System.out.println("與第一次輸入的不同!");
 		}
-		
-	
+		System.out.println("密碼正確");
 	}
 
 }
